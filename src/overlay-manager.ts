13aceb046194a3a62e47b85c1ef6fa8d818396f3
Iftach Yakar <DeDuckProject@users.noreply.github.com>
2017-12-04 17:08:45 +0200

    turned inline hide style into class (#6)

--- src/overlay-manager.ts
+++ src/overlay-manager.ts
@@ -8,15 +8,17 @@ let overlayCounter = 0;
 export const OVERLAY_LAYERS_CLASS = `overlay-layers`;
 export const CONTENT_LAYERS_CLASS = `content-layer`;
 export const PORTAL_ROOT_CLASS = `portal-root`;
+export const HIDE_PORTAL_STYLE = `hide-portal`;
 
-const hideStyle = "position:absolute;visibility:hidden; transform:unset; pointer-events:none;width:100%;height:100%;top:0;left:0;";
+const css = `.${HIDE_PORTAL_STYLE}{position:absolute;visibility:hidden; transform:unset; pointer-events:none;width:100%;height:100%;top:0;left:0}`;
+let style:HTMLStyleElement;
 
 export class OverlayManager {
     private domMirror:DOMMirror = new DOMMirror(filterIdAndOn);
 
-    private portalRoot:HTMLDivElement = createHTML(`<div class="${PORTAL_ROOT_CLASS}" data-automation-id="${PORTAL_ROOT_CLASS}"></div>`) as HTMLDivElement;
-    private contentLayer:HTMLDivElement = createHTML(`<div class="${CONTENT_LAYERS_CLASS}" data-automation-id="${CONTENT_LAYERS_CLASS}"></div>`) as HTMLDivElement;
-    private overlayLayer:HTMLDivElement = createHTML(`<div class="${OVERLAY_LAYERS_CLASS}" data-automation-id="${OVERLAY_LAYERS_CLASS}"></div>`) as HTMLDivElement;
+    private portalRoot:HTMLDivElement = createHTML(`<div class="${PORTAL_ROOT_CLASS} ${HIDE_PORTAL_STYLE}" data-automation-id="${PORTAL_ROOT_CLASS}"></div>`) as HTMLDivElement;
+    private contentLayer:HTMLDivElement = createHTML(`<div class="${CONTENT_LAYERS_CLASS} ${HIDE_PORTAL_STYLE}" data-automation-id="${CONTENT_LAYERS_CLASS}"></div>`) as HTMLDivElement;
+    private overlayLayer:HTMLDivElement = createHTML(`<div class="${OVERLAY_LAYERS_CLASS} ${HIDE_PORTAL_STYLE}" data-automation-id="${OVERLAY_LAYERS_CLASS}"></div>`) as HTMLDivElement;
 
     private overlays:{[s:string]:{parent:string|null, layer:Element}} = {};
     constructor(
@@ -25,7 +27,13 @@ export class OverlayManager {
         this.portalRoot.appendChild(this.contentLayer);
         this.portalRoot.appendChild(this.overlayLayer); //Overlay should be after content
 
-        this.portalRoot.setAttribute('style',hideStyle);
+        // this.portalRoot.setAttribute('class',HIDE_PORTAL_STYLE);
+        if (style==undefined){
+            let s = document.createElement('style');
+            s.textContent = css;
+            style = s;
+            document.head.appendChild(style);
+        }
         root.appendChild(this.portalRoot);
     }
 
@@ -43,14 +51,16 @@ export class OverlayManager {
 
     public createOverlay(overlayContext:Element):OverlayItem {
         let id = overlayCounter++;
-        const overlay = createHTML(`<div class="overlay" data-automation-id="overlay" style="${hideStyle}" data-overlay-id="${id}"></div>`);
+        const overlay = createHTML(`<div class="overlay ${HIDE_PORTAL_STYLE}" data-automation-id="overlay" data-overlay-id="${id}"></div>`);
         const {overlayTop, overlayTarget} = this.mirrorParentChain(overlayContext);
         
         if(!overlayTop || !overlayTarget){
             throw new Error('create overlay fail');
         }
 
-        overlayContext.setAttribute('style',hideStyle+"display:none;");
+        overlayContext.setAttribute('style',"display:none;");
+        let orgClassName = overlayContext.getAttribute('class') || "";
+        overlayContext.setAttribute('class',orgClassName+" "+HIDE_PORTAL_STYLE);
 
         overlay.appendChild(overlayTop);
         this.overlayLayer.appendChild(overlay);
@@ -77,7 +87,8 @@ export class OverlayManager {
         while(currentSource && currentSource !== this.root){
             const mirror = this.domMirror.mirrorNode(currentSource);
             if(prevMirror){
-                mirror.setAttribute('style',hideStyle); //hide chain except source
+                let orgClassName = mirror.getAttribute('class')||"";
+                mirror.setAttribute('class',orgClassName+" "+HIDE_PORTAL_STYLE); //hide chain except source
                 mirror.appendChild(prevMirror);
             } else {
                 overlayTarget = mirror;
