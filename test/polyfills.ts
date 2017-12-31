83342c411122885c66410f6878c093ca98a69660
Iftach Yakar <fatach@gmail.com>
2017-12-31 14:31:26 +0200

    fix 'matches' polyfill for chrome mobile

--- test/polyfills.ts
+++ test/polyfills.ts
@@ -1,6 +1,8 @@
 import 'core-js/shim';
 
-// matcher 'match' in tests uses Element matches which doesn't go by that name in ie.
+// matcher 'match' in tests uses Element matches which doesn't go by that name in ie and chrome mobile.
 // see https://developer.mozilla.org/en-US/docs/Web/API/Element/matches
-if (!Element.prototype.matches)
-    Element.prototype.matches = Element.prototype.msMatchesSelector;
+if (!Element.prototype.matches) {
+    Element.prototype.matches =
+        Element.prototype.msMatchesSelector || Element.prototype.webkitMatchesSelector;
+}
