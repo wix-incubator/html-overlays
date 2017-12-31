2da37e80fb481f4a973e4ec1b59e146ab66a6b78
Iftach Yakar <fatach@gmail.com>
2017-12-31 14:41:30 +0200

    fix 'matches' polyfill for platforms

--- test/polyfills.ts
+++ test/polyfills.ts
@@ -3,6 +3,17 @@ import 'core-js/shim';
 // matcher 'match' in tests uses Element matches which doesn't go by that name in ie and chrome mobile.
 // see https://developer.mozilla.org/en-US/docs/Web/API/Element/matches
 if (!Element.prototype.matches) {
-    Element.prototype.matches =
-        Element.prototype.msMatchesSelector || Element.prototype.webkitMatchesSelector;
+    const elemProto = Element.prototype as any;
+    elemProto.matches =
+        elemProto.matchesSelector ||
+        elemProto.mozMatchesSelector ||
+        elemProto.msMatchesSelector ||
+        elemProto.oMatchesSelector ||
+        elemProto.webkitMatchesSelector ||
+        function(this:any,s:string) {
+            var matches = (this.document || this.ownerDocument).querySelectorAll(s),
+                i = matches.length;
+            while (--i >= 0 && matches.item(i) !== this) {}
+            return i > -1;
+        };
 }
