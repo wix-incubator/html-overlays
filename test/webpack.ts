db07c9a701ea72b287127e96aae16874af964cb0
Iftach Yakar <DeDuckProject@users.noreply.github.com>
2018-01-02 20:17:48 +0200

    add matches polyfill to run in CI as well (#11)
    fixed 'matches' polyfill in IE

--- test/webpack.ts
+++ test/webpack.ts
@@ -1,3 +1,4 @@
+import './polyfills';   // TODO better move import of this to webpack-config like before...
 // https://webpack.js.org/guides/dependency-management/#require-context
 // the following is used an an entry point for karma (that runs karma-webpack)
 
